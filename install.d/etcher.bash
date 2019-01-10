66936cc44f976aa6f9018b990e30709d790a39d8
Ferry Schoenmakers <f.b.f.schoenmakers@gmail.com>
2019-01-10 10:12:30 +0100

    Add etcher install script

---
+++ install.d/etcher.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+listfile="/etc/apt/sources.list.d/etcher.list"
+
+if [ ! -f $listfile ]
+then
+    echo "Adding etcher ppa"
+
+    # Required for adding https sources to apt
+    sudo apt-get install apt-transport-https
+
+    echo -e "deb https://deb.etcher.io stable etcher" | sudo tee $listfile
+
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
+
+    sudo apt-get update
+
+    sudo apt-get install --assume-yes etcher-electron
+fi
