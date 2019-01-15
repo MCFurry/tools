56ccd35adc9c31c800b008ca2014fa3ddd7b49a2
Ferry Schoenmakers <f.b.f.schoenmakers@gmail.com>
2019-01-15 12:35:46 +0100

    Added install script for diff-so-fancy

---
+++ install.d/diff-so-fancy.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -O https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
+chmod +x diff-so-fancy
+sudo mv diff-so-fancy /usr/local/bin/
+
+# Other diff-so-fancy is set up in the git terminal setup
