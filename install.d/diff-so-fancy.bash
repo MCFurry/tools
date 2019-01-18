9243116e2174d14f7ccf007de79f957c99f51155
Ferry Schoenmakers <f.b.f.schoenmakers@gmail.com>
2019-01-18 21:18:59 +0100

    Tools update since some did not install correctly

--- install.d/diff-so-fancy.bash
+++ install.d/diff-so-fancy.bash
@@ -4,4 +4,4 @@ curl -O https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_pa
 chmod +x diff-so-fancy
 sudo mv diff-so-fancy /usr/local/bin/
 
-# Other diff-so-fancy is set up in the git terminal setup
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
--- install.d/fzf.bash
+++ install.d/fzf.bash
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-# Find out latest fzf release
-# Thanks to: https://github.com/lukechilds
-get_fzf_latest_release() {
-  curl --silent "https://api.github.com/repos/junegunn/fzf-bin/releases/latest" |
-  grep '"tag_name":' |
-  sed -E 's/.*"([^"]+)".*/\1/'
-}
-
-VER=$(get_fzf_latest_release)
-
-wget https://github.com/junegunn/fzf-bin/releases/download/${VER}/fzf-${VER}-linux_amd64.tgz
-tar -zxvf fzf-${VER}-linux_amd64.tgz
-sudo mv fzf /usr/local/bin/
-rm fzf-${VER}-linux_amd64.tgz
+git clone --depth 1 https://github.com/junegunn/fzf.git /tmp/.fzf
+/tmp/.fzf/install
