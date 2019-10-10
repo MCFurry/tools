f3fe539052d40721a621aeb35af5174deaf8f2ae
mcfurry <f.b.f.schoenmakers@gmail.com>
2019-10-10 15:12:20 +0200

    Use snap for vscode installation and install my favourite extenstions

--- install.d/vscode.bash
+++ install.d/vscode.bash
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb
-sudo dpkg -i vscode.deb
-rm vscode.deb
+# Install vscode via snap
+snap install code --classic
+sudo apt install default-jre
+
+# Install ros extensions
+code --install-extension ms-vscode.cpptools
+code --install-extension austin.code-gnu-global
+code --install-extension ms-python.python
+code --install-extension redhat.vscode-yaml
+code --install-extension coenraadS.bracket-pair-colorizer-2
+code --install-extension gruntfuggly.todo-tree
+code --install-extension atishay-Jain.All-Autocomplete
+
