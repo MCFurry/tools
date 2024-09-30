e3ea1d6b01b8ca4ce9ed12c71d2e2de36df0d802
Ferry Schoenmakers <MCFurry@users.noreply.github.com>
2024-09-30 15:11:36 +0200

    Update diff-so-fancy.bash

--- install.d/diff-so-fancy.bash
+++ install.d/diff-so-fancy.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -O https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
+curl -O https://raw.githubusercontent.com/so-fancy/diff-so-fancy/refs/heads/next/diff-so-fancy
 chmod +x diff-so-fancy
 sudo mv diff-so-fancy /usr/local/bin/
 
